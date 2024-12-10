a1855a10bcd4c551b96e95dcf314f5be5e586fc6
Sam Warren <samwarren4@gmail.com>
2024-12-10 10:47:25 -0800

    create-react-app

--- api/README.md
+++ api/README.md
@@ -1,3 +1,11 @@
 To run the project:
 1. `pip install requirements.txt`
 2. `python manage.py runserver`
+
+API Request Limits:
+Moxfield: 1 request per second
+Topdeck: 200 requests per minute
+
+Documentation:
+Topdeck: https://topdeck.gg/docs/tournaments-v2
+Scryfall: https://scryfall.com/docs/api
---
+++ ui/cedhtools/.eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // Disable the rule that requires React in scope
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
