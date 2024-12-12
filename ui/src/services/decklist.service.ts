6dfc43067a6ad7af8063cafd7b62f07f78d93c9a
Sam Warren <samwarren4@gmail.com>
2024-12-11 16:25:24 -0800

    Add TODO

--- ui/src/services/decklist.service.ts
+++ ui/src/services/decklist.service.ts
@@ -2,7 +2,7 @@ import axios, { AxiosError } from 'axios';
 
 const BASE_URL = import.meta.env.VITE_CEDHTOOLS_API_BASE_URL;
 
-export async function getDecklistById(id: string): Promise<any> {
+export async function getDecklistById(id: string): Promise<any> { // TODO: Define type for promise
   const url = `${BASE_URL}/api/moxfield/deck/${id}`;
   try {
     const response = await axios.get(url, { withCredentials: true });
