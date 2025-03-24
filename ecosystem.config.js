954e04c47c2289155a363e5c8cf5f37a8673f48b
Sam Warren <sam@junctiontech.ca>
2025-03-24 17:00:45 -0600

    Update ETL worker

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'cedhtools-worker',
-    script: 'cedhtools/client/dist/etl/worker.js',
+    script: 'client/dist/etl/worker.js',
     instances: 1,
     autorestart: true,
     watch: false,
